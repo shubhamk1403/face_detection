aa464ec2695c6072b03ad3f7b085d71af64747e4
Shubham Kumar <shubhamk1403@gmail.com>
2018-03-05 23:38:38 +0530

    Create face_detect.py

---
+++ face_detect.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+
+faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(gray,1.3,5)
+
+    # Draw a rectangle around the faces
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y), (x+w,y+h), (255,0,0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes=eye_cascade.detectMultiScale(roi_gray)
+        for(ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+    cv2.imshow('frame',frame)
+    k=cv2.waitKey(30) & 0xff
+    if k==27:
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
